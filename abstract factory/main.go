423b9f1e246376c35be64d07bc1c01e736e10381
Nurkhat <113333558+nktwn@users.noreply.github.com>
2023-11-21 23:50:26 +0600

    k

--- abstract factory/main.go
+++ abstract factory/main.go
@@ -1,1 +1,65 @@
-package abstract_factory
+package main
+
+import "fmt"
+
+// Button интерфейс для продукта типа "Кнопка".
+type Button interface {
+	Paint() string
+}
+
+// WinButton конкретная реализация кнопки для Windows.
+type WinButton struct{}
+
+func (b *WinButton) Paint() string {
+	return "Windows Button"
+}
+
+// MacButton конкретная реализация кнопки для MacOS.
+type MacButton struct{}
+
+func (b *MacButton) Paint() string {
+	return "MacOS Button"
+}
+
+// GUIFactory интерфейс абстрактной фабрики для создания элементов GUI.
+type GUIFactory interface {
+	CreateButton() Button
+}
+
+// WinFactory конкретная фабрика для создания элементов GUI для Windows.
+type WinFactory struct{}
+
+func (f *WinFactory) CreateButton() Button {
+	return &WinButton{}
+}
+
+// MacFactory конкретная фабрика для создания элементов GUI для MacOS.
+type MacFactory struct{}
+
+func (f *MacFactory) CreateButton() Button {
+	return &MacButton{}
+}
+
+// Application структура, использующая абстрактную фабрику.
+type Application struct {
+	factory GUIFactory
+}
+
+func (a *Application) CreateUI() {
+	button := a.factory.CreateButton()
+	fmt.Println(button.Paint())
+}
+
+func main() {
+	var factory GUIFactory
+
+	// Конфигурация для Windows
+	factory = &WinFactory{}
+	app := Application{factory: factory}
+	app.CreateUI()
+
+	// Конфигурация для MacOS
+	factory = &MacFactory{}
+	app = Application{factory: factory}
+	app.CreateUI()
+}
--- factory/main.go
+++ factory/main.go
@@ -1,1 +1,53 @@
-package factory
+package main
+
+import "fmt"
+
+// Product определяет интерфейс продукта, который фабрика будет создавать.
+type Product interface {
+	Use() string
+}
+
+// ConcreteProductA конкретная реализация продукта A.
+type ConcreteProductA struct{}
+
+func (p *ConcreteProductA) Use() string {
+	return "Product A is used"
+}
+
+// ConcreteProductB конкретная реализация продукта B.
+type ConcreteProductB struct{}
+
+func (p *ConcreteProductB) Use() string {
+	return "Product B is used"
+}
+
+// Factory определяет интерфейс фабрики с методом CreateProduct.
+type Factory interface {
+	CreateProduct(productType string) Product
+}
+
+// ConcreteFactory конкретная реализация фабрики.
+type ConcreteFactory struct{}
+
+func (f *ConcreteFactory) CreateProduct(productType string) Product {
+	switch productType {
+	case "A":
+		return &ConcreteProductA{}
+	case "B":
+		return &ConcreteProductB{}
+	default:
+		return nil
+	}
+}
+
+func main() {
+	factory := &ConcreteFactory{}
+
+	// Создание продукта типа A
+	productA := factory.CreateProduct("A")
+	fmt.Println(productA.Use())
+
+	// Создание продукта типа B
+	productB := factory.CreateProduct("B")
+	fmt.Println(productB.Use())
+}
